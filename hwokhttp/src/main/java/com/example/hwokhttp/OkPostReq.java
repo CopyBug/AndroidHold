c3555ca7f2e01ac450f72bc11d23eb5e22465777
a785661432@163.com <a785661432@163.com>
2020-07-13 10:47:19 +0800

    修复了dataBinding

--- hwokhttp/src/main/java/com/example/hwokhttp/OkPostReq.java
+++ hwokhttp/src/main/java/com/example/hwokhttp/OkPostReq.java
@@ -1,32 +1,100 @@
 package com.example.hwokhttp;
 
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 /**
  * Time       : 2020/07/10 下午1:54
  * Author     : sion
  * Description:
  */
-public class OkPostReq extends OkReq {
-    private final Request.Builder builder;
-    private OkHttpClient okHttpClient;
+public class OkPostReq<T> extends OkReq<T> {
 
-    public OkPostReq(OkHttpClient okHttpClient, String url) {
-        this.okHttpClient = okHttpClient;
-        builder = new Request.Builder().url(url);
 
+    public OkPostReq(OkHttpClient okHttpClient) {
+        super(okHttpClient);
     }
 
-    public void addHeader(Map<String, String> header) {
-        for (Map.Entry<String, String> reqHeaders : header.entrySet()) {
-            String headerKey = reqHeaders.getKey();
-            String headerValue = reqHeaders.getKey();
-            builder.addHeader(headerKey,headerValue);
+
+    public <T> Observable<T> doPostFromRx() {
+        return Observable.create(new ObservableOnSubscribe<T>() {
+            @Override
+            public void subscribe(ObservableEmitter<T> emitter) {
+                doPost(formBody(), emitter);
+            }
+        });
+    }
+
+    public <T> Observable<T> doPostJsonRx() {
+        return Observable.create(new ObservableOnSubscribe<T>() {
+            @Override
+            public void subscribe(ObservableEmitter<T> emitter) {
+                doPost(jsonBody(), emitter);
+            }
+        });
+    }
+
+    protected <T> void doPost(RequestBody reqBody, final ObservableEmitter<T> emitter) {
+        okHttpClient.newCall(builder.post(reqBody).build())
+                .enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        Throwable throwable = new Throwable(e.getMessage() + "");
+                        emitter.onError(throwable);
+                        emitter.onComplete();
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) {
+                        try {
+                            String json = response.body().string();
+                            emitter.onNext((T) new Gson().fromJson(json, t.getClass()));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            emitter.onError(new Throwable(e.getMessage() + ""));
+                        }
+                    }
+                });
+    }
+
+    public FormBody formBody() {
+        FormBody.Builder formBody = new FormBody.Builder();
+        for (Map.Entry<String, String> reqParam : this.param.entrySet()) {
+            formBody.add(reqParam.getKey(), reqParam.getValue());
         }
+        return formBody.build();
     }
 
+    public RequestBody jsonBody() {
+        String jsonBody = new Gson().toJson(param);
+        return RequestBody.create(MediaType.parse("application/json; charset=utf-8"), jsonBody);
+    }
 
+    public void multipartBody(String key,File value){
+        MultipartBody.Builder multipartBody = new MultipartBody.Builder();
+        for (Map.Entry<String, String> entry : param.entrySet()) {
+            multipartBody.addFormDataPart(entry.getKey(),entry.getValue());
+        }
+//        multipartBody.addFormDataPart(key,value.getName(),)
+
+    }
 }
--- hwokhttp/src/main/java/com/example/hwokhttp/OkReq.java
+++ hwokhttp/src/main/java/com/example/hwokhttp/OkReq.java
@@ -1,10 +1,46 @@
 package com.example.hwokhttp;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 /**
  * Time       : 2020/07/10 下午1:54
  * Author     : sion
  * Description:
  */
-public class OkReq {
+public abstract class OkReq<T> {
+    protected  T t;
+    protected final Request.Builder builder;
+    protected Map<String, String> param;
+    protected OkHttpClient okHttpClient;
+    protected String reqUrl;
+
+    public OkReq(OkHttpClient okHttpClient) {
+        this.okHttpClient = okHttpClient;
+        builder = new Request.Builder();
+        param = new HashMap<>();
+    }
+
+
+    public void setReqUrl(String reqUrl) {
+        this.reqUrl = reqUrl;
+        builder.url(this.reqUrl);
+    }
+
+    public void addHeader(Map<String, String> header) {
+        for (Map.Entry<String, String> reqHeaders : header.entrySet()) {
+            String headerKey = reqHeaders.getKey();
+            String headerValue = reqHeaders.getKey();
+            builder.addHeader(headerKey, headerValue);
+        }
+    }
+
+    public OkReq param(String key, String value) {
+        param.put(key, value);
+        return this;
+    }
 
 }
