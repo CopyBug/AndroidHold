19556e296f1cac21ab181476aef37d83cdc34125
sion <a785661432@163.com>
2020-08-03 14:14:49 +0800

    完善了网络请求

---
+++ image/build/generated/ap_generated_sources/debug/out/com/hjq/copy/BR.java
@@ -0,0 +1,5 @@
+package com.hjq.copy;
+
+public class BR {
+  public static int _all = 0;
+}
---
+++ image/build/generated/source/dataBinding/trigger/debug/com/hjq/copy/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package com.hjq.copy;
+
+import androidx.databinding.BindingBuildInfo;
+
+@BindingBuildInfo
+public class DataBindingInfo {}
